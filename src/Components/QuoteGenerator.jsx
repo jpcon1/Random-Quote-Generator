80ffa8065ca810d3b13e77423cf252387f43944d
jpcon1 <josephpauloconcepcion@gmail.com>
2024-02-07 18:09:26 +0800

    need  more styles

--- src/Components/QuoteGenerator.jsx
+++ src/Components/QuoteGenerator.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { gsap } from "gsap/dist/gsap";
+import { useGSAP } from "@gsap/react";
+import { TweenMax } from "gsap/gsap-core";
+import { TweenLite } from "gsap/gsap-core";
+
 
 export default function QuoteGenerator(){
 
@@ -10,6 +14,10 @@ export default function QuoteGenerator(){
     const [author,setAuthor] = useState("")
 
 
+    useGSAP(()=>{
+            gsap.fromTo('.author', { rotation: 0 }, { rotation: 360, duration: 1.2 });
+            gsap.fromTo('.word', { opacity: 0 }, { opacity: 1, duration: 1.2 });
+        },{dependencies:[word]})
 
 
 
