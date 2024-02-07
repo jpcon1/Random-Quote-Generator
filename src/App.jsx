fb287a709d9c1d8decbb64ef05b608dbfa7d3580
jpcon1 <josephpauloconcecpcion.com>
2024-02-07 13:31:11 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
 
   return (
     <>
-    <QuoteGenerator></QuoteGenerator>
-    
+
+    <QuoteGenerator/>
+
     </>
   )
 }
--- src/Components/QuoteGenerator.jsx
+++ src/Components/QuoteGenerator.jsx
@@ -1,39 +1,68 @@
-import { useState } from "react";
-
-
+import { useEffect, useState } from "react";
+import { animated, useSpring } from '@react-spring/web'
 
 
 
 
 export default function QuoteGenerator(){
 
-
     const url = "https://api.quotable.io/random"
+
     const [word,setWord] = useState("")
+
     const [author,setAuthor] = useState("")
 
-   let getQuote = ()=>{
-        fetch(url)
+    const [springs, api] = useSpring(() => ({
+        from: { x: 0 },
+      }))
+
+      const handleClick = () => {
+        api.start({
+          from: {
+            x: 0,
+          },
+          to: {
+            x: 100,
+          },
+        })
+      }
+
+   function getQuote(){
+    fetch(url)
+
         .then((data)=>data.json())
+
         .then((item)=>{
+
             setWord(item.content)
+
             setAuthor(item.author)
+
         })
    }
 
-
-
-
-
     return(
     <>
-    <div className="quote-box">
-        <h1>Random Quote Generator</h1>
-        <p>{word}</p>
-        <p>-{author}</p>
-        <button onClick={getQuote}>Get Quote</button>
 
-        
+    <animated.div
+      style={{
+        width: 80,
+        height: 80,
+        background: '#ff6d6d',
+        borderRadius: 8,
+        ...springs,
+      }}
+      />
+    <div className="quote-box">
+
+        <h1>Random Quote Generator</h1>
+
+        <p>{word}</p>
+
+        <p>-{author}</p>
+
+        <button onClick={getQuote}>Get Quote</button>
+        <button onClick={handleClick}>animate</button>
     </div>
     </>)
 }
